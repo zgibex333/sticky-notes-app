97e547801e6190a093ca5e0d10c5ffdb3a87057f
Den CZ <zgibex333@gmail.com>
2022-10-28 14:12:26 +0300

    db added

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3500;
-const { logger } = require("./middleware/logger");
-const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
+const connectDB = require("./config/dbConnection");
+const mongoose = require("mongoose");
+
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 const corsOptions = require("./config/corsOptions");
+
+const app = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
@@ -32,6 +41,17 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
