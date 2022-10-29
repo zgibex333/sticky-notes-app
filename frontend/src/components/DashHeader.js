12b3bf5100eec3f328a493279c93c21e39dbb8f8
Den CZ <zgibex333@gmail.com>
2022-10-29 18:57:16 +0300

    ui and routing without logic

---
+++ frontend/src/components/DashHeader.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const DashHeader = () => {
+  const content = (
+    <header className="dash-header">
+      <div className="dash-header__container">
+        <Link>
+          <h1 className="dash-header__title">techNotes</h1>
+        </Link>
+        <nav className="dash-header__nav">{/* nav btns later */}</nav>
+      </div>
+    </header>
+  );
+  return content;
+};
+
+export default DashHeader;
